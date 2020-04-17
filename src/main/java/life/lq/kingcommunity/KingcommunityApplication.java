b892aa49d9f00efb54d03dd1b0e980c251720372
lqdeGitHub <1115282097@qq.com>
2020-04-17 17:12:40 +0800

    init repo

---
+++ src/main/java/life/lq/kingcommunity/KingcommunityApplication.java
@@ -0,0 +1,14 @@
+package life.lq.kingcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingcommunityApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(KingcommunityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/life/lq/kingcommunity/controller/HelloController.java
@@ -0,0 +1,19 @@
+package life.lq.kingcommunity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        String s = "hello," + name;
+        return s;
+    }
+}
