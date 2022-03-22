0e2f2d2ba1339c441ebe0b9e7ae17fba70d9cb96
lqdeGitHub <11152820972qq.com>
2022-03-22 10:28:33 +0800

    extract github config from code to properties

--- src/main/java/life/lq/kingcommunity/controller/AuthorizeController.java
+++ src/main/java/life/lq/kingcommunity/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.lq.kingcommunity.dto.AccessTokenDTO;
 import life.lq.kingcommunity.dto.GithubUser;
 import life.lq.kingcommunity.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,13 +15,20 @@ public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String secret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code){
         AccessTokenDTO dto = new AccessTokenDTO();
-        dto.setClient_id("bc3705d443e9ac5ff1a4");
-        dto.setClient_secret("6d57c80617873a9001f3d7af16cc27893793cf06");
+        dto.setClient_id(clientId);
+        dto.setClient_secret(secret);
         dto.setCode(code);
-        dto.setRedirect_uri("http://localhost:8887/callback");
+        dto.setRedirect_uri(redirectUri);
         String accessToken = gitHubProvider.getAccessToken(dto);
         GithubUser user = gitHubProvider.getUser(accessToken);
         System.out.println(user.getName());
